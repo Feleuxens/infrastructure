cc0f99d3a32d1d6450fb130f3d55bac937ea5beb
Feleuxens <felixgrueb@icloud.com>
2023-06-08 17:39:28 +0200

    Add actions and cerium environment

---
+++ environments/cerium/main.tf
@@ -0,0 +1,4 @@
+locals {
+  domain = "feleuxens.de"
+}
+
---
+++ environments/cerium/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    helm = {
+      source = "hashicorp/helm"
+    }
+  }
+
+  backend "http" {
+    address        = "https://api.tfstate.dev/github/v1"
+    lock_address   = "https://api.tfstate.dev/github/v1/lock"
+    unlock_address = "https://api.tfstate.dev/github/v1/lock"
+    lock_method    = "PUT"
+    unlock_method  = "DELETE"
+    username       = "feleuxens/feleuxens.de-prod-tfstate"
+  }
+}
