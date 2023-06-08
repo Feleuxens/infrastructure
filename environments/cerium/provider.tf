76e0517968a665a128b95ac9d9d521225763057f
Feleuxens <felixgrueb@icloud.com>
2023-06-08 18:23:56 +0200

    Use secrets

--- environments/cerium/provider.tf
+++ environments/cerium/provider.tf
@@ -1,3 +1,15 @@
 provider "kubernetes" {
+    host = var.host
+    client_certificate = base64decode(var.cerium_client_cert)
+    client_key = base64decode(var.cerium_client_key)
+    cluster_ca_certificate = base64decode(var.cerium_cluster_ca_cert)
+}
 
+provider "helm" {
+    kubernetes {
+        host = var.host
+        client_certificate = base64decode(var.cerium_client_cert)
+        client_key = base64decode(var.cerium_client_key)
+        cluster_ca_certificate = base64decode(var.cerium_cluster_ca_cert)
+    }
 }
---
+++ environments/cerium/variables.tf
@@ -0,0 +1,23 @@
+variable "cerium_host" {
+    description = "Kubernetes host"
+    type = string
+    default = ""
+}
+
+variable "cerium_client_cert" {
+    description = "Kubernetes client certificate"
+    type = string
+    default = ""
+}
+
+variable "cerium_client_key" {
+    description = "Kubernetes client key"
+    type = string
+    default = ""
+}
+
+variable "cerium_cluster_ca_cert" {
+    description = "Kubernetes cluster certificate authoritate certificate"
+    type = string
+    default = ""
+}
